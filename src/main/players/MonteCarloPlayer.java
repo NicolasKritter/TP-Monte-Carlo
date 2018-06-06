5868d2020401dfb9100f6c9cd1963aefd5fadcc4
Nicolas Kritter <nicolas.kritter@isep.fr>
2018-06-06 19:07:50 +0200

    remove

--- src/main/players/MonteCarloPlayer.java
+++ src/main/players/MonteCarloPlayer.java
@@ -19,35 +19,6 @@ public class MonteCarloPlayer extends Player{
 		
 	}
 
-	private int searchVictory(int[][] board,List<int[]> todo,boolean turn) {
-		switch(Gomoku.evaluate(board)) {
-		case -1:
-			return 0;
-		case 1:
-			return 1;
-		case  2:
-			return -1;
-		case 0: 
-			break;
-		}
-		int[][] board2 = Utils.copyBoard(board);
-		int victory = 0;
-		List<int[]> todo2 = new ArrayList<int[]>();
-		todo2.addAll(todo);
-		for (int[] c:todo) {
-			todo2.remove(c);
-			if (turn) {
-				board2[c[0]][c[1]] = 1;
-			}else {
-				board2[c[0]][c[1]] = 2;
-			}
-			turn = !turn;
-			victory += searchVictory(board2,todo2,turn);
-			
-		}	
-		return victory;
-	}
-
 	private int[] computeNextMove(int[][] board) {
 		int score =0;
 		int[] res = {-1,-1};
